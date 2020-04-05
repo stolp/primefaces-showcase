bd26f89f6ae27227c39b9edcb45f89ac4a8fbe64
Christoph Straßer <christoph.strasser78@gmail.com>
2020-04-05 09:13:36 +0200

    FileUpload: Chunked

--- src/main/java/org/primefaces/showcase/view/file/FileUploadView.java
+++ src/main/java/org/primefaces/showcase/view/file/FileUploadView.java
@@ -17,12 +17,19 @@ package org.primefaces.showcase.view.file;
 
 import org.primefaces.event.FileChunkUploadEvent;
 import org.primefaces.event.FileUploadEvent;
-import org.primefaces.model.file.*;
+import org.primefaces.model.file.UploadedFile;
+import org.primefaces.model.file.UploadedFileChunk;
+import org.primefaces.model.file.UploadedFiles;
 
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 @Named
 @RequestScoped
@@ -30,6 +37,9 @@ public class FileUploadView {
     
     private UploadedFile file;
     private UploadedFiles files;
+
+    private boolean writeChunkUpload2TempDir = false;
+    String tempDir = System.getProperty("java.io.tmpdir");
 
     public UploadedFile getFile() {
         return file;
@@ -71,10 +81,40 @@ public class FileUploadView {
                 " - " + uploadedFileChunk.getChunkRangeEnd() +
                 " of file " + uploadedFileChunk.getFileName() + " is uploaded.");
         FacesContext.getCurrentInstance().addMessage(null, msg);
+
+        if (writeChunkUpload2TempDir) {
+            try {
+                OpenOption openOption = StandardOpenOption.APPEND;
+                if (uploadedFileChunk.getChunkRangeBegin() == 0) {
+                    openOption = StandardOpenOption.CREATE;
+                }
+
+                Files.write(Paths.get(tempDir, uploadedFileChunk.getFileName()), uploadedFileChunk.getContent(), openOption);
+            }
+            catch (IOException ex) {
+                msg = new FacesMessage("Chunk Error", "Chunk " + uploadedFileChunk.getChunkRangeBegin() +
+                        " - " + uploadedFileChunk.getChunkRangeEnd() +
+                        " of file " + uploadedFileChunk.getFileName() + " can´t be written to tempDir. " + ex.getMessage());
+                msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+                FacesContext.getCurrentInstance().addMessage(null, msg);
+            }
+        }
     }
     
     public void handleFileUpload(FileUploadEvent event) {
         FacesMessage msg = new FacesMessage("Successful", event.getFile().getFileName() + " is uploaded.");
         FacesContext.getCurrentInstance().addMessage(null, msg);
+
+        if (writeChunkUpload2TempDir) {
+            try {
+                //cleanup tempDir
+                Files.delete(Paths.get(tempDir, event.getFile().getFileName()));
+            }
+            catch (IOException ex) {
+                msg = new FacesMessage("Error", event.getFile().getFileName() + " can´t be deleted from tempDir. " + ex.getMessage());
+                msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+                FacesContext.getCurrentInstance().addMessage(null, msg);
+            }
+        }
     }
 }
