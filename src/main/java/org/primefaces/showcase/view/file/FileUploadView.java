0852633dedfe88251c73cfe817eaa081cafd4787
Thomas Andraschko <tandraschko@apache.org>
2020-08-21 15:34:03 +0200

    #1077

--- src/main/java/org/primefaces/showcase/view/file/FileUploadView.java
+++ src/main/java/org/primefaces/showcase/view/file/FileUploadView.java
@@ -23,6 +23,7 @@ import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import org.primefaces.event.FilesUploadEvent;
 
 @Named
 @RequestScoped
@@ -64,7 +65,14 @@ public class FileUploadView {
     }
 
     public void handleFileUpload(FileUploadEvent event) {
-        FacesMessage msg = new FacesMessage("Successful", event.getFile().getFileName() + " is uploaded.");
-        FacesContext.getCurrentInstance().addMessage(null, msg);
+        FacesMessage message = new FacesMessage("Successful", event.getFile().getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+
+    public void handleFilesUpload(FilesUploadEvent event) {
+        for (UploadedFile f : event.getFiles().getFiles()) {
+            FacesMessage message = new FacesMessage("Successful", f.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
     }
 }
