993556b646bfc52f16d910a25efd2d09709e06d7
burak <m.buunsal@gmail.com>
2020-10-26 10:23:07 +0300

    add product service & update carousel

--- src/main/java/org/primefaces/showcase/view/data/CarouselView.java
+++ src/main/java/org/primefaces/showcase/view/data/CarouselView.java
@@ -1,23 +1,10 @@
-/*
- * Copyright 2009-2014 PrimeTek.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.primefaces.showcase.view.data;
 
 import javax.faces.view.ViewScoped;
 import org.primefaces.showcase.domain.Car;
+import org.primefaces.showcase.domain.Product;
 import org.primefaces.showcase.service.CarService;
+import org.primefaces.showcase.service.ProductService;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
@@ -29,31 +16,31 @@ import java.util.List;
 @ViewScoped
 public class CarouselView implements Serializable {
     
-    private List<Car> cars;
+    private List<Product> products;
     
-    private Car selectedCar;
+    private Product selectedProduct;
     
     @Inject
-    private CarService service;
+    private ProductService service;
     
     @PostConstruct
     public void init() {
-        cars = service.createCars(9);
+        products = service.getProductsWithSize(9);
     }
 
-    public List<Car> getCars() {
-        return cars;
+    public List<Product> getProducts() {
+        return products;
     }
 
-    public void setService(CarService service) {
-        this.service = service;
+    public void setProducts(List<Product> products) {
+        this.products = products;
     }
 
-    public Car getSelectedCar() {
-        return selectedCar;
+    public Product getSelectedProduct() {
+        return selectedProduct;
     }
 
-    public void setSelectedCar(Car selectedCar) {
-        this.selectedCar = selectedCar;
+    public void setSelectedProduct(Product selectedProduct) {
+        this.selectedProduct = selectedProduct;
     }
 }
