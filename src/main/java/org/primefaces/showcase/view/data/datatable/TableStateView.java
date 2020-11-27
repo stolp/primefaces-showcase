82b98d2a5503910e9b59c41858c9f84e0182bd58
Sébastien Lepage <sebastien.lepage@outlook.com>
2020-11-27 22:14:20 +0100

    Use ViewScoped for TableStateView.java

--- src/main/java/org/primefaces/showcase/view/data/datatable/TableStateView.java
+++ src/main/java/org/primefaces/showcase/view/data/datatable/TableStateView.java
@@ -24,13 +24,14 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.util.List;
 
 @Named("dtTableStateView")
-@SessionScoped
+@ViewScoped
 public class TableStateView implements Serializable {
     
     private List<Car> cars;
