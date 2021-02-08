43c9b56fde0354b819fa7ef92a928e748e3019f1
burak <m.buunsal@gmail.com>
2021-02-08 15:14:02 +0300

    add default filter demo

--- src/main/java/org/primefaces/showcase/view/data/datatable/FilterView.java
+++ src/main/java/org/primefaces/showcase/view/data/datatable/FilterView.java
@@ -24,6 +24,8 @@
 package org.primefaces.showcase.view.data.datatable;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
@@ -34,6 +36,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.primefaces.model.FilterMeta;
+import org.primefaces.model.MatchMode;
 import org.primefaces.showcase.domain.Customer;
 import org.primefaces.showcase.domain.CustomerStatus;
 import org.primefaces.showcase.domain.Representative;
@@ -49,18 +52,38 @@ public class FilterView implements Serializable {
 
     private List<Customer> customers1;
 
-    private List<Customer> filteredCustomers1;
-    
     private List<Customer> customers2;
 
+    private List<Customer> customers3;
+
+    private List<Customer> filteredCustomers1;
+
     private List<Customer> filteredCustomers2;
+
+    private List<Customer> filteredCustomers3;
 
     private List<FilterMeta> filterBy;
 
     @PostConstruct
     public void init() {
         customers1 = service.getCustomers(10);
-        customers2 = service.getCustomers(10);
+        customers2 = service.getCustomers(50);
+        customers3 = service.getCustomers(10);
+
+        filterBy = new ArrayList<>();
+
+        filterBy.add(FilterMeta.builder()
+                .field("status")
+                .filterValue(CustomerStatus.NEW)
+                .matchMode(MatchMode.EQUALS)
+                .build());
+
+        filterBy.add(FilterMeta.builder()
+                .field("date")
+                .filterValue(Arrays.asList(LocalDate.now().minusDays(28), LocalDate.now().plusDays(28)))
+                .matchMode(MatchMode.RANGE)
+                .build());
+
     }
     
     public boolean globalFilterFunction(Object value, Object filter, Locale locale) {
@@ -74,6 +97,7 @@ public class FilterView implements Serializable {
         return customer.getName().toLowerCase().contains(filterText)
                 || customer.getCountry().getName().toLowerCase().contains(filterText)
                 || customer.getRepresentative().getName().toLowerCase().contains(filterText)
+                || customer.getDate().toString().toLowerCase().contains(filterText)
                 || customer.getStatus().name().toLowerCase().contains(filterText)
                 || customer.getActivity() < filterInt;
     }
@@ -99,24 +123,36 @@ public class FilterView implements Serializable {
         return customers1;
     }
 
+    public List<Customer> getCustomers2() {
+        return customers2;
+    }
+
+    public List<Customer> getCustomers3() {
+        return customers3;
+    }
+
     public List<Customer> getFilteredCustomers1() {
         return filteredCustomers1;
     }
 
-    public void setFilteredCustomers2(List<Customer> filteredCustomers2) {
-        this.filteredCustomers2 = filteredCustomers2;
-    }
-    
-    public List<Customer> getCustomers2() {
-        return customers2;
+    public void setFilteredCustomers1(List<Customer> filteredCustomers1) {
+        this.filteredCustomers1 = filteredCustomers1;
     }
 
     public List<Customer> getFilteredCustomers2() {
         return filteredCustomers2;
     }
 
-    public void setFilteredCustomers1(List<Customer> filteredCustomers1) {
-        this.filteredCustomers1 = filteredCustomers1;
+    public void setFilteredCustomers2(List<Customer> filteredCustomers2) {
+        this.filteredCustomers2 = filteredCustomers2;
+    }
+
+    public List<Customer> getFilteredCustomers3() {
+        return filteredCustomers3;
+    }
+
+    public void setFilteredCustomers3(List<Customer> filteredCustomers3) {
+        this.filteredCustomers3 = filteredCustomers3;
     }
 
     public void setService(CustomerService service) {
